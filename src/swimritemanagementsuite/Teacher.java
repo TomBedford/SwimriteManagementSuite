18dad083690ff2bba13b16fe18dc5225eab08933
Bedford <Bedford@10.211.55.4>
2014-12-22 17:34:19 +0000

    completed the Teacher class.

---
+++ src/swimritemanagementsuite/Teacher.java
@@ -0,0 +1,66 @@
+package swimritemanagementsuite;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+/**
+ * This class represents the Teacher database table, declaring its fields and 
+ * specific attributes of the fields. Accessor and mutator methods are also 
+ * available for each of the fields (except a mutator for the auto generated ID).
+ * 
+ * @author Thomas Bedford (m2081433)
+ */
+@DatabaseTable(tableName = "Teacher")
+public class Teacher {
+    
+    /**
+     * Primary Key: Auto generated Teacher ID number.
+     */
+    @DatabaseField(generatedId = true)
+    private int teacherId;
+    
+    /**
+     * The name of the Teacher (field cannot be null and must be unique).
+     */
+    @DatabaseField (columnName = "Teacher Name", canBeNull = false, unique = true)
+    private String teacherName;
+
+    /**
+     * Default constructor of the Teacher class.
+     */
+    public Teacher() {
+        // ORMLite needs a no-arg constructor
+    }
+
+    /**
+     * Parameterized constructor to create a new Teacher in the Teacher database.
+     * @param teacherName The individual Teachers name.
+     */
+    public Teacher(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    /**
+     * Accessor to retrieve the teachers auto generated Id.
+     * @return teacherId The auto generated teachers Id.
+     */
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    /**
+     * Accessor to retrieve the teachers name.
+     * @return teacherName The teachers name.
+     */
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    /**
+     * Mutator to change the teachers name.
+     * @param teacherName The teachers updated name. 
+     */
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+}
